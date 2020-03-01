b736138ede5b83db7bb70b40e8b545cf52cd16d2
songgf <song761212_160906@qq.com>
2020-03-01 15:27:49 +0800

    AIO

---
+++ src/main/java/com/boatfly/java8/nio/TestNonBlockingNIO3.java
@@ -0,0 +1,18 @@
+package com.boatfly.java8.nio;
+
+import org.junit.Test;
+
+/**
+ * DatagramChannel udp
+ */
+public class TestNonBlockingNIO3 {
+    @Test
+    public void send(){
+
+    }
+
+    @Test
+    public void receive(){
+
+    }
+}
