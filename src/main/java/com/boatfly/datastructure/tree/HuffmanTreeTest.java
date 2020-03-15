318f76b98be75f0361ba047a334654e61d0dc598
songgf <song761212_160906@qq.com>
2020-03-15 20:45:41 +0800

    HuffmanTree

---
+++ src/main/java/com/boatfly/datastructure/tree/HuffmanTreeTest.java
@@ -0,0 +1,16 @@
+package com.boatfly.datastructure.tree;
+
+/**
+ * Huffman Tree 赫夫曼树 ：WPL最小的树
+ * 带权路径长度最短的树，权值较大的节点离根最近。
+ *
+ * - 路径
+ * - 路径长度
+ * - 节点的权 值
+ * - 带权路径长度 L-1
+ * - 节点的带权路径长度：路径长度x节点的权
+ *
+ * - 树的带权路径长度WPL weighted path length :所有的叶子节点的带权路径长度之和
+ */
+public class HuffmanTreeTest {
+}
